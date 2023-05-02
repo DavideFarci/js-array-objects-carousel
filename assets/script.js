7bfcf0d36674e0f06c9f3264491d7b2223e9b673
DavideFarci <yank92.df@gmail.com>
2023-05-02 17:46:03 +0200

    add setInterval 5sec

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,6 @@
 /*
 Quando l'utente preme uno dei bottoni, la classe active passa da un'immagine all'altra 
 */
-// const arrImages = [
-//     `assets/img/01.jpg`,
-//     `assets/img/02.jpg`,
-//     `assets/img/03.jpg`,
-//     `assets/img/04.jpg`,
-//     `assets/img/05.jpg`,
-// ];
 
 const images = [
     {
@@ -36,6 +29,17 @@ const images = [
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
 ];
+
+setInterval(() => {
+    listHighlighted[activeIndex].classList.remove("active");
+    listThumbs[activeIndex].classList.remove("active");
+    activeIndex++;
+    if (activeIndex >= listHighlighted.length) {
+        activeIndex = 0;
+    }
+    listHighlighted[activeIndex].classList.add("active");
+    listThumbs[activeIndex].classList.add("active");
+}, 5000);
 
 const containerHighlighted = document.querySelector(".highlighted");
 const containerThumbs = document.querySelector(".thumbs")
