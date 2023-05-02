288b1d0df4040448ff5ec699194d2e3f6e33d71c
DavideFarci <yank92.df@gmail.com>
2023-05-02 14:28:27 +0200

    add new array

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,36 @@
 /*
 Quando l'utente preme uno dei bottoni, la classe active passa da un'immagine all'altra 
 */
-const arrImages = [
-    `assets/img/01.jpg`,
-    `assets/img/02.jpg`,
-    `assets/img/03.jpg`,
-    `assets/img/04.jpg`,
-    `assets/img/05.jpg`,
+// const arrImages = [
+//     `assets/img/01.jpg`,
+//     `assets/img/02.jpg`,
+//     `assets/img/03.jpg`,
+//     `assets/img/04.jpg`,
+//     `assets/img/05.jpg`,
+// ];
+
+const images = [
+    {
+        image: 'img/01.webp',
+        title: 'Marvel\'s Spiderman Miles Morale',
+        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+    }, {
+        image: 'img/02.webp',
+        title: 'Ratchet & Clank: Rift Apart',
+        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+    }, {
+        image: 'img/03.webp',
+        title: 'Fortnite',
+        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+    }, {
+        image: 'img/04.webp',
+        title: 'Stray',
+        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+    }, {
+        image: 'img/05.webp',
+        title: "Marvel's Avengers",
+        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+    }
 ];
 
 const containerHighlighted = document.querySelector(".highlighted");
